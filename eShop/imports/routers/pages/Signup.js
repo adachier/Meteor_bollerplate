0a881cf822f380479a66c84652d044f21de9375c
adachier <57669264+adachier@users.noreply.github.com>
2020-01-23 12:47:26 +0100

    Add SignUp form via semantic-ui

--- eShop/imports/routers/pages/Signup.js
+++ eShop/imports/routers/pages/Signup.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Form, Button, Container} from 'semantic-ui-react'
 
 function Signup(props){
     return(
-        <h1>Test avec 1 seul E</h1>
+        <Container>
+            <h1>Sign Up for FREE !</h1>
+            <Form>
+                <Form.Input label="Your e-mail" required type="email" placeholder="example@mail.com" />
+                <Form.Input label="Your password" required type="password" placeholder="password" />
+                <Button color="orange">Sign Up</Button>
+            </Form>
+        </Container>
     )
 }
 
