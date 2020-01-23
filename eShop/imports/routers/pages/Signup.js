3032a84d687cba7a4af9ecac6a93a69a8d83617c
adachier <57669264+adachier@users.noreply.github.com>
2020-01-23 15:12:51 +0100

    Ajout de l'accounts-password

--- eShop/imports/routers/pages/Signup.js
+++ eShop/imports/routers/pages/Signup.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, Button, Container} from 'semantic-ui-react'
+import {Accounts} from 'meteor/accounts-base'
 
 function Signup(props){
+    const [email, setEmail]                     =useState()
+    const [password, setPassword]               =useState()
+
+    const signup = () => {
+
+    }
     return(
         <Container>
             <h1>Sign Up for FREE !</h1>
             <Form>
-                <Form.Input label="Your e-mail" required type="email" placeholder="example@mail.com" />
-                <Form.Input label="Your password" required type="password" placeholder="password" />
-                <Button color="orange">Sign Up</Button>
+                <Form.Input value ={email} onChange={(e, {value}) => setEmail(value)} label="Your e-mail" required type="email" placeholder="example@mail.com" />
+                <Form.Input value ={password} onChange={(e, {value}) => setPassword(value)} label="Your password" required type="password" placeholder="password" />
+                <Button disabled={!email || !password} color="orange">Sign Up</Button>
             </Form>
         </Container>
     )
